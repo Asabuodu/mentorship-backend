454aa0cde1b771e2bbbf1677e06062ff595f486d
Innocent <99240481+Asabuodu@users.noreply.github.com>
2025-07-09 13:54:16 +0100

    still working on cors

--- src/index.ts
+++ src/index.ts
@@ -69,9 +69,22 @@ const allowedOrigins = [
 ];
 
 
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   })
 );
